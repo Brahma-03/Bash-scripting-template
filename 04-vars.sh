8bb90263c8d3506cfa607d384a8e8761551595b3
Chandra Mouliswara <peaswarchandra@gmail.com>
2023-08-21 19:35:40 +0530

    gg

--- 04-vars.sh
+++ 04-vars.sh
@@ -3,6 +3,8 @@ a=10
 
 b=abc
 
+LOGGEDIN-USERS=$(who | wc -l)
+
 #a is  10 is integer
 
 echo value of a is : $a
@@ -14,4 +16,6 @@ Dte=$(date +%F)
 
 echo $Dte
 
+
+echo Number of logged in users are $LOGGEDIN-USERS
 echo value of e is ${e}
