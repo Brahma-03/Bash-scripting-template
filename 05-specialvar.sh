776ee84e0ca4097ce66f1d6b4aa8b8ae4474fb2b
Chandra Mouliswara <peaswarchandra@gmail.com>
2023-08-22 18:47:18 +0530

    welcome

--- 05-specialvar.sh
+++ 05-specialvar.sh
@@ -1,6 +1,6 @@
 # Here are special variables
 
-#$0    : This will gives the name of the start you are runnin
+#$0    : This will gives the name of the script that you are running
 
 
 #$1 to $9 :  you can pass a maximum of 9 variables from the command line when you are running the script
@@ -17,23 +17,15 @@ echo "script name that you are running is $0"
 
 a=10
 b=$1
+c=$2
+d=$3
 
 echo value of a is : $a
 echo value od b is : $b
+echo value of c is : $c
+echo value of d is : $d
 
-
-
-
-
-
-
-
-
-
-
-
-.................
-
+# sh scriptname.sh 100 200 300 
 
 
 
