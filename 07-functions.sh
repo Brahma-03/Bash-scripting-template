d9b2e3d0ec25e36c234e4939341521005989c78b
Chandra Mouliswara <peaswarchandra@gmail.com>
2023-08-23 20:23:45 +0530

    lll

--- 07-functions.sh
+++ 07-functions.sh
@@ -6,4 +6,14 @@ echo "hai fro the sample function"
 echo "i will be printing the uptime"
 uptime
 }
- 
+
+stat()
+
+{
+    echo "Load Average on the system from last 1 min is : $(uptime |awk -f : '{print $NF}' | awk -f  '{print $1}')"
+    echo "Number of users signed in is : $(who|wc-1)"
+
+    echo "start Function is completed  bye"
+}
+
+
