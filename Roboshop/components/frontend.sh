531c615cfe2acd52fccdde387d3710ae5fef6ed1
Chandra Mouliswara <peaswarchandra@gmail.com>
2023-09-12 07:37:37 +0530

    ggg

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "I am installing frontend"
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+curl -s -L -o /tmp/frontennd.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginxx/default.d/roboshop.conf
+
+systemctl restart nginx
+
