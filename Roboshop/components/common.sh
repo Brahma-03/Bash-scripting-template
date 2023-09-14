bba8df516d42f811f683e5b34b22c36b87186a6c
Chandra Mouliswara <peaswarchandra@gmail.com>
2023-09-14 07:19:13 +0530

    kkk

--- Roboshop/components/common.sh
+++
@@ -1,9 +0,0 @@
-
-stat()
-{
-if [$1 -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-    else
-    echo -e "\e[31m failure. Look for the logs \e[0m"
-    fi
-}
--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -2,19 +2,29 @@
 
 set -e 
 
-source components/common.sh
-
 echo -n "Instslling Nginx :"
 yum install nginx -y   >> /tmp/frontend.log
 systemctl enable nginx
 
 echo -n "Starting nginx:"
 systemctl start nginx
-stat $?
+
+if [$1 -eq 0 ] ; 
+then
+    echo -e "\e[32m Success \e[0m"
+    else
+    echo -e "\e[31m failure. Look for the logs \e[0m"
+    fi
 
 echo -n "Downloading tthe code"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-stat $?
+
+if [$1 -eq 0 ] ; 
+then
+    echo -e "\e[32m Success \e[0m"
+    else
+    echo -e "\e[31m failure. Look for the logs \e[0m"
+    fi
 
 cd /usr/share/nginx/html
 rm -rf *
@@ -26,4 +36,10 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 systemctl restart nginx
 echo -n "completed SETUP"
-stat $?
+
+if [$1 -eq 0 ] ; 
+then
+    echo -e "\e[32m Success \e[0m"
+    else
+    echo -e "\e[31m failure. Look for the logs \e[0m"
+    fi
